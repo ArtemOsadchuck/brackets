504621a471b4e8c13c71b2983e948d9adcd6460c
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2020-10-09 14:20:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0) return false
+  let brecetsArr = bracketsConfig.map(a=>a.join(""))
+  // let arr = [...str]
+  for (let i = 0; i < brecetsArr.length; i++){
+    if (str.includes(brecetsArr[i])){
+      str= str.replace(brecetsArr[i], "")
+      i = i-brecetsArr.length
+    }
+      
+  }
+  console.log(str)
+  
+  return str.length === 0 ? true : false  
 }
